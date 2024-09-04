2af87f7d3c621cf547cd7057ad0c852a8adc6d26
sayyedarib <sayyedaribhussain4321@gmail.com>
2024-09-04 16:48:45 +0530

    create course-card , fetch data and render it

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,10 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import type { CourseCardProps } from "@/interface";
+
+import { readCourses } from "@/db/queries";
+import { ThreeDCourseCard } from "@/components/course-card";
 import SearchBar from "./search-bar";
 
-export default function Header() {
+export default function Hero() {
+  const [courses, setCourses] = useState<CourseCardProps[] | null>(null);
+
+  useEffect(() => {
+    readCourses().then((data) => {
+      console.log(data);
+      setCourses(data);
+    });
+  }, []);
+
   return (
     <div className="flex flex-col gap-16 items-center">
       <SearchBar />
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-2" />
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        {courses?.map((course) => (
+          <ThreeDCourseCard key={course.id} {...course} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/interface/index.ts
@@ -0,0 +1,9 @@
+export interface CourseCardProps {
+  id: number;
+  name: string;
+  description: string;
+  thumbnail: string;
+  duration: number;
+  location: string;
+  open: boolean;
+}
