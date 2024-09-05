7b03b7d977fd1397d9d0083e0159909838f68d23
sayyedarib <sayyedaribhussain4321@gmail.com>
2024-09-05 20:03:30 +0530

    refactor: Update course-card component to use dynamic URL for course details

--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -38,7 +38,7 @@ export function ThreeDCourseCard(courseCardProps: CourseCardProps) {
           <CardItem
             translateZ={20}
             as={Link}
-            href="https://twitter.com/mannupaaji"
+            href={`/courses/${courseCardProps.id}`}
             target="__blank"
             className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
           >
