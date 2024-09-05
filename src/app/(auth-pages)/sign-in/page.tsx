f2c559a3059c9145cb32c29b44c8dab7f4e875f6
sayyedarib <sayyedaribhussain4321@gmail.com>
2024-09-06 00:08:28 +0530

    feat: dummy user login in dev env

--- src/app/(auth-pages)/sign-in/page.tsx
+++ src/app/(auth-pages)/sign-in/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { signInAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
@@ -7,37 +10,59 @@ import Link from "next/link";
 
 export default function Login({ searchParams }: { searchParams: Message }) {
   return (
-    <form className="flex-1 flex flex-col min-w-64">
-      <h1 className="text-2xl font-medium">Sign in</h1>
-      <p className="text-sm text-foreground">
-        Don't have an account?{" "}
-        <Link className="text-foreground font-medium underline" href="/sign-up">
-          Sign up
-        </Link>
-      </p>
-      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
-        <div className="flex justify-between items-center">
-          <Label htmlFor="password">Password</Label>
-          <Link
-            className="text-xs text-foreground underline"
-            href="/forgot-password"
+    <div className="w-screen h-screen flex items-center justify-center">
+      {/* TODO: ENV is undefined on client side that's during rendering it shows dev environment always, fix it later */}
+      {process.env.ENV == "production" ? (
+        <form className="flex flex-col min-w-80">
+          <h1 className="text-2xl font-medium">Sign in</h1>
+          <p className="text-sm text-foreground">
+            Don't have an account?{" "}
+            <Link
+              className="text-foreground font-medium underline"
+              href="/sign-up"
+            >
+              Sign up
+            </Link>
+          </p>
+          <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+            <Label htmlFor="email">Email</Label>
+            <Input name="email" placeholder="you@example.com" required />
+            <div className="flex justify-between items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link
+                className="text-xs text-foreground underline"
+                href="/forgot-password"
+              >
+                Forgot Password?
+              </Link>
+            </div>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              required
+            />
+            <SubmitButton pendingText="Signing In..." formAction={signInAction}>
+              Sign in
+            </SubmitButton>
+            <FormMessage message={searchParams} />
+          </div>
+        </form>
+      ) : (
+        <div className="flex">
+          {/* Singin as dummy user */}
+          <Button
+            onClick={() => {
+              const formData = new FormData();
+              formData.append("email", "dummy@example.com");
+              formData.append("password", "password");
+              signInAction(formData);
+            }}
           >
-            Forgot Password?
-          </Link>
+            Sign in as Student
+          </Button>
         </div>
-        <Input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          required
-        />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-          Sign in
-        </SubmitButton>
-        <FormMessage message={searchParams} />
-      </div>
-    </form>
+      )}
+    </div>
   );
 }
