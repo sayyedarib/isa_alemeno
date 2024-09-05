1f0af0934c6c02855ea8f8fcaafc8031e87a8d6b
sayyedarib <sayyedaribhussain4321@gmail.com>
2024-09-05 23:38:55 +0530

    add student-dasboard structure

---
+++ src/app/student-dashboard/[views]/page.tsx
@@ -0,0 +1,24 @@
+
+
+export default function Page({ params }: { params: { views: string } }) {
+        function capitalizeFirstLetter(str: string): string {
+      if (!str) return str;
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+  return (
+    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+      <div className="flex items-center">
+        <h1 className="text-lg font-semibold md:text-2xl">{capitalizeFirstLetter(params.views)}</h1>
+      </div>
+      <div
+        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+        x-chunk="dashboard-02-chunk-1"
+      >
+        <div className="flex flex-col items-center gap-1 text-center">
+            Under construction
+        </div>
+      </div>
+    </main>
+  );
+}
