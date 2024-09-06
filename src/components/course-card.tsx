5c1353e085272e49d29d032a14dfc8f2743e0395
sayyedarib <sayyedaribhussain4321@gmail.com>
2024-09-06 12:20:02 +0530

    add badge for admission status

--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -9,6 +9,7 @@ import type { CourseCardProps } from "@/interface";
 
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import { createClient } from "@/utils/supabase/client";
+import { Badge } from "@/components/ui/badge"; // Import the Badge component from ShadCN
 
 interface ExtendedCourseCardProps extends CourseCardProps {
   enrolled: boolean | undefined;
@@ -33,12 +34,24 @@ export function ThreeDCourseCard(courseCardProps: ExtendedCourseCardProps) {
 
   return (
     <CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+        {courseCardProps.open ? (
+          <Badge>Open for Admission</Badge>
+        ) : (
+          <Badge variant={"destructive"}>Closed for Admission</Badge>
+        )}
         <CardItem
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
           {courseCardProps.name}
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 font-semibold py-1 px-2 bg-gray-100 dark:bg-gray-800 rounded-md"
+        >
+          by {courseCardProps.instructorName}
         </CardItem>
         <CardItem
           as="p"
@@ -63,7 +76,7 @@ export function ThreeDCourseCard(courseCardProps: ExtendedCourseCardProps) {
             href={`/courses/${courseCardProps.id}`}
             className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
           >
-            See Deatils →
+            See Details →
           </CardItem>
           <CardItem
             onClick={handleClick}
